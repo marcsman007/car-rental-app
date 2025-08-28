a520348d4c523efbef44b72684cc8ed0491a101b
marcsman007 <macaburasmarcjayson@gmail.com>
2025-08-28 17:44:26 +0800

    Revert "UPdated server.js to disable serving frontend."
    This reverts commit d3c31eedc297680d08c3df851e0c3b07861a82fc.

--- backend/server.js
+++ backend/server.js
@@ -38,13 +38,12 @@ app.use('/api/cars', carRoutes);
 app.use('/api/bookings', bookingRoutes);
 
 // --- Serve React Frontend ---
-// const frontendBuildPath = path.join(__dirname, '../frontend/build');
-// app.use(express.static(frontendBuildPath));
+const frontendBuildPath = path.join(__dirname, '../frontend/build');
+app.use(express.static(frontendBuildPath));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(frontendBuildPath, 'index.html'));
-// });
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(frontendBuildPath, 'index.html'));
+});
 
 // --- Start Server ---
 const PORT = process.env.PORT || 5000;
