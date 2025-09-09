54f55235e707051cbf01b1cd5549a778c8544900
marcsman007 <macaburasmarcjayson@gmail.com>
2025-09-09 20:54:28 +0800

    Update for AWS simple deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      ssl: true,                 // ✅ ensures TLS/SSL
+      ssl: false,                 // ✅ ensures TLS/SSL
       serverSelectionTimeoutMS: 5000
     });
     console.log('MongoDB connected');
