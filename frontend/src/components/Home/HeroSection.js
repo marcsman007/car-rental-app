a9df67178a417c745ca0788fbee2157afdd0821b
marcsman007 <macaburasmarcjayson@gmail.com>
2025-10-15 17:20:55 +0800

    CI/CD test

--- frontend/src/components/Home/HeroSection.js
+++ frontend/src/components/Home/HeroSection.js
@@ -6,7 +6,7 @@ function HeroSection({ token }) {
     <div className="w-full max-w-7xl mx-auto mt-8 px-4 sm:px-6 lg:px-8">
       <div className="bg-gradient-to-r from-orange-400 via-red-400 to-pink-500 text-white py-20 px-6 sm:px-12 lg:px-16 rounded-3xl shadow-md flex flex-col items-center text-center">
         <h1 className="text-4xl sm:text-5xl font-bold mb-4">
-          Welcome to Car Rental App 🚗
+          Welcome to Car ni Grace 🚗
         </h1>
         <p className="text-lg sm:text-xl mb-6">
           Browse our cars and book your next adventure with ease.
